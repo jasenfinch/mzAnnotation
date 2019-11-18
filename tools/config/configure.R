2931f9e8a18040bb53bb03943caa099c25e9c6e4
jasenfinch <jsf9@aber.ac.uk>
2019-11-18 15:21:24 +0000

    remove configure scripts

--- tools/config/configure.R
+++
@@ -1,6 +0,0 @@
-# Prepare your package for installation here.
-# Use 'define()' to define configuration variables.
-# Use 'configure_file()' to substitute configuration values.
-
-configure_file("src/Makevars.win")
-configure_file("src/Makevars.in")
