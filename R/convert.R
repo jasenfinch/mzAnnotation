f75e107960fa7479656a8b953571e7bfbef2e19c
jasenfinch <jsf9@aber.ac.uk>
2020-01-10 01:18:39 +0000

    convert now accepts a vector of inputs

--- R/convert.R
+++ R/convert.R
@@ -1,19 +1,20 @@
 #' convert
-#' @description convert between SMILES and Inchi and to InchiKey
-#' @param input a valid SMILE or Inchi
+#' @description convert between SMILES and InChI and to InChIKey
+#' @param input vecotr of valid SMILES or InChIs
 #' @param inputType either "smiles" or "inchi", denoting the input type
 #' @param outputType either "smiles", "inchi" or "inchikey", denoting the output type
 #' @examples
 #' convert(aminoAcids$SMILES[1],'smiles','inchi')
-#' @importFrom callr r
 #' @importFrom utils getFromNamespace
 #' @importFrom webchem cs_convert
 #' @export
 
 convert <- function(input, inputType, outputType) {
   
-  suppressMessages({
-    output <- cs_convert(input,from = inputType,to = outputType)[[1]]
+  output <- map_chr(input,~{
+    suppressMessages({
+      cs_convert(.x,from = inputType,to = outputType)[[1]]
+    })
   })
   
   return(output)
